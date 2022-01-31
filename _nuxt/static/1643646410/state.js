ffa67ee724724e53cfdb5e0c80565cf2c9459cf2
DESKTOP-HDUTHV5\Shayne <shayne.plesanaden@gmail.com>
2022-01-31 09:27:57 -0700

    Updates

--- _nuxt/static/1643646410/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsg-builder\u002F_nuxt\u002Fstatic\u002F1643646410",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsg-builder\u002F",assetsPath:"\u002Fsg-builder\u002F_nuxt\u002F",cdnURL:a}}}}(null));
